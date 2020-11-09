a68770d606d6b63e4ef463433a667e608951160a
Ernest Cheung <ErnestCheung02@gmail.com>
2020-11-08 16:13:21 -0800

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -5,3 +5,28 @@ class BankAccount:
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = balance
+
+    def deposit(amount):
+        amount_deposit = amount + self.balance
+        print(f"Amount Deposited: ${amount_deposit}")
+
+    def withdraw(amount):
+        amount_withdraw = self.balance - amount
+        if amount_withdraw <= 0:
+            self.balance - 10
+            print('Insufficient funds.')
+        else:
+            print(f'Amount Withdrawn: ${amount_withdraw}')
+
+    def get_balance():
+        print(f'Your balance is: {self.balance}')
+        return self.balance
+
+    def add_interest():
+        interest = self.balance * 0.00083
+
+    def print_receipt():
+        print(f'{self.full_name}')
+        print(f'Account No.: {self.account_number}')
+        print(f'Routing No.: {self.routing_number}')
+        print(f'Balance: ${float(self.balance)}')
