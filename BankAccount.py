894351e6dd572200debf06d234960a25379f292a
Ernest Cheung <ErnestCheung02@gmail.com>
2020-11-09 14:49:23 -0800

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,3 +1,11 @@
+from random import choice
+
+lol = ['1', '2', '3']
+
+for i in lol:
+    print(choice(i))
+
+
 class BankAccount:
 
     def __init__(self, full_name, account_number, routing_number, balance):
@@ -6,11 +14,11 @@ class BankAccount:
         self.routing_number = routing_number
         self.balance = balance
 
-    def deposit(amount):
+    def deposit(self, amount):
         amount_deposit = amount + self.balance
         print(f"Amount Deposited: ${amount_deposit}")
 
-    def withdraw(amount):
+    def withdraw(self, amount):
         amount_withdraw = self.balance - amount
         if amount_withdraw <= 0:
             self.balance - 10
@@ -18,15 +26,18 @@ class BankAccount:
         else:
             print(f'Amount Withdrawn: ${amount_withdraw}')
 
-    def get_balance():
+    def get_balance(self):
         print(f'Your balance is: {self.balance}')
         return self.balance
 
-    def add_interest():
+    def add_interest(self):
         interest = self.balance * 0.00083
 
-    def print_receipt():
+    def print_receipt(self):
         print(f'{self.full_name}')
         print(f'Account No.: {self.account_number}')
         print(f'Routing No.: {self.routing_number}')
         print(f'Balance: ${float(self.balance)}')
+
+
+bing_bong = BankAccount('Bing Bong', 17701342, 123456789, 0)
