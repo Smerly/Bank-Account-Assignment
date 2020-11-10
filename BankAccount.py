07edd6aa4be055762153575b57864e33f72c3533
Ernest Cheung <ErnestCheung02@gmail.com>
2020-11-09 18:05:10 -0800

    Final Thing

--- BankAccount.py
+++ BankAccount.py
@@ -1,11 +1,3 @@
-from random import choice
-
-lol = ['1', '2', '3']
-
-for i in lol:
-    print(choice(i))
-
-
 class BankAccount:
 
     def __init__(self, full_name, account_number, routing_number, balance):
@@ -15,29 +7,53 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        amount_deposit = amount + self.balance
-        print(f"Amount Deposited: ${amount_deposit}")
+        self.balance += amount
+        print(f"Amount Deposited: ${self.balance}")
 
     def withdraw(self, amount):
-        amount_withdraw = self.balance - amount
-        if amount_withdraw <= 0:
-            self.balance - 10
+        self.balance -= amount
+        if self.balance <= 0:
+            self.balance -= 10
             print('Insufficient funds.')
         else:
-            print(f'Amount Withdrawn: ${amount_withdraw}')
+            print(f'Amount Withdrawn: ${amount}')
 
     def get_balance(self):
-        print(f'Your balance is: {self.balance}')
+        print(f'Your balance is: ${self.balance}')
         return self.balance
 
     def add_interest(self):
-        interest = self.balance * 0.00083
+        self.balance = round(self.balance * 1.00083, 2)
 
     def print_receipt(self):
+        safe = []
+        for i in range(len(str(self.account_number))):
+            # Take
+            if i <= 3:
+
+                safe.append('*')
+            elif i > 3:
+                safe.append(str(self.account_number)[i])
+        asterisks = ''.join(safe)
+
         print(f'{self.full_name}')
-        print(f'Account No.: {self.account_number}')
+        print(f'Account No.: {asterisks}')
         print(f'Routing No.: {self.routing_number}')
         print(f'Balance: ${float(self.balance)}')
 
 
-bing_bong = BankAccount('Bing Bong', 17701342, 123456789, 0)
+bing_bong = BankAccount('Bing Bong', 17742013, 123456789, 0)
+
+bing_bong.deposit(100)
+
+bing_bong.get_balance()
+
+bing_bong.withdraw(50)
+
+bing_bong.get_balance()
+
+bing_bong.add_interest()
+
+bing_bong.get_balance()
+
+bing_bong.print_receipt()
