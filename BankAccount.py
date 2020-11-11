180377b1f517cf703bceacce877919df1e34ad73
Ernest Cheung <ErnestCheung02@gmail.com>
2020-11-10 17:20:38 -0800

    Added more comments

--- BankAccount.py
+++ BankAccount.py
@@ -25,15 +25,24 @@ fullNumbaFinal = int(fullNumbaString)
 # Class
 
 class BankAccount:
+
+    # Properties
+
     def __init__(self, full_name, account_number, routing_number, balance):
         self.full_name = full_name
         self.account_number = account_number
         self.routing_number = routing_number
         self.balance = 0
 
+    # Methods
+
+        # Allows an amount to be deposited
+
     def deposit(self, amount):
         self.balance += amount
         print(f"Amount Deposited: ${self.balance}")
+
+        # Allows an amount to be withdrawn
 
     def withdraw(self, amount):
         self.balance -= amount
@@ -43,12 +52,18 @@ class BankAccount:
         else:
             print(f'Amount Withdrawn: ${amount}')
 
+        # Checks the balance of your bank account
+
     def get_balance(self):
         print(f'Your balance is: ${self.balance}')
         return self.balance
 
+        # Adds the monthly interest to your bank account
+
     def add_interest(self):
         self.balance = round(self.balance * 1.00083, 2)
+
+        # Prints your receipt
 
     def print_receipt(self):
 
@@ -63,12 +78,15 @@ class BankAccount:
                 safe.append(str(self.account_number)[i])
         asterisks = ''.join(safe)
 
+        # The printing of the receipt (added the prints with nothing to make indents to make it look nicer)
+        print('')
         print(f'{self.full_name}')
         print(f'Account No.: {asterisks}')
         print(f'Routing No.: {self.routing_number}')
         print(f'Balance: ${float(self.balance)}')
+        print('')
 
-# Making it work
+# Making it work or testing it
 
 
 bing_bong = BankAccount('Bing Bong', fullNumbaFinal, 123456789, 0)
